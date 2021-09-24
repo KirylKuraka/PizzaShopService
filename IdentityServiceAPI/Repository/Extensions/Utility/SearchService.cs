a3c7ff58155dc7aab2fa2fa5c2fcb536a4661836
Kirill Kurako <kiryl.kuraka@innowise-group.com>
2021-09-24 13:34:39 +0300

    added a class for searching by the properties of the input class

---
+++ IdentityServiceAPI/Repository/Extensions/Utility/SearchService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Repository.Extensions.Utility
+{
+    public static class SearchService
+    {
+        public static int SearchByProperties<T>(string searchTerm, PropertyInfo[] propertyInfos, T inputClass)
+        {
+            int propertyCount = 0;
+            foreach (var property in propertyInfos)
+            {
+                object value = property.GetValue(inputClass, null);
+                if (value != null)
+                {
+                    Regex regex = new Regex("^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$");
+                    Match match = regex.Match(value.ToString());
+                    if (!regex.Match(value.ToString()).Success)
+                    {
+                        if (!value.ToString().ToLower().Contains(searchTerm.Trim().ToLower()))
+                        {
+                            propertyCount++;
+                        }
+                    }
+                    else
+                    {
+                        propertyCount++;
+                    }
+                }
+                else
+                {
+                    propertyCount++;
+                }
+            }
+
+            return propertyCount;
+        }
+    }
+}
